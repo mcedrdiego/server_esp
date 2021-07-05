f46fee8b4ae92083bdcc048b759574cbb44cd1c3
Cedric Diego <cedricdiego0@gmail.com>
2021-07-05 10:20:52 +0200

    python server check

--- socket_server.py
+++ socket_server.py
@@ -1,33 +1,26 @@
 import asyncio
 import websockets
 
-f = open('data.txt', 'w')
-
-async def consumer(data):
-	for x in list(data):
-		f.write(str(x))
-		f.write(',  \n')
-
-async def consumer_handler(websocket, path):
-	message = await websocket.recv()
-	await consumer(message)
-
-async def handler(websocket, path):
-	#data = await websocket.recv()	
-	#print(f"<<< {list(data)}")
-	consumer_task = asyncio.ensure_future(
-		consumer_handler(websocket, path))
-	
-	done, pending = await asyncio.wait(
-		[consumer_task], 
-		return_when=asyncio.FIRST_COMPLETED,
-	)
-	
-	for task in pending:
-		task.cancel()
-
-async def main():
-	async with websockets.serve(handler, "172.31.0.160", 8080):
-		await asyncio.Future()  # run forever
+class Server(object):
+	def __init__(self, host, port):
+		self.host, self.port = host, port
+		self.loop = asyncio.get_event_loop()
 		
-asyncio.run(main()) 
+		self.stop = self.loop.create_future()
+		
+		self.loop.run_until_complete(self.server())
+	
+	async def server(self):
+		async with websockets.serve(self.ws_handler, self.host, self.port):
+			await self.stop
+	
+	async def ws_handler(self, websocket, path):
+		msg = await websocket.recv()
+		print(f'< {msg}')
+		
+		await websocket.send(msg)
+		print(f'> {msg}')
+		
+if __name__ == '__main__':
+	server = Server(host='172.31.0.160', port=8080)
+

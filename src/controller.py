c21ce3b703b5edf26075606622035633315b8e2d
Cedric Diego <cedricdiego0@gmail.com>
2021-07-05 11:28:20 +0200

    gui

---
+++ src/controller.py
@@ -0,0 +1,80 @@
+#from server import Server
+import sys
+from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtCore import QObject, Signal, QProcess
+
+from main_window import MainWindow
+#from server import Server
+
+class Controller:
+    def __init__(self) -> None:
+        # Use QApplication(sys.argv) to allow command line 
+        # else use QApplication([])
+        self.app = QApplication(sys.argv)
+        self.main_window = MainWindow(controller=self)
+        #self.server = Server(controller=self)
+        self.p = None
+
+    def run_app(self):
+        self.main_window.show()
+        return self.app.exec_()
+
+    def data_ready_callback(self, arg):
+        pass
+    
+    def message(self, s):
+        self.main_window.control_panel.raw_data_panel.txt_box.appendPlainText(s)
+        self.main_window.screen.update()
+
+    def start_process(self):
+        if self.p is None:  # No process running.
+            self.message("Executing process")
+            self.p = QProcess()  # Keep a reference to the QProcess (e.g. on self) while it's running.
+            self.p.readyReadStandardOutput.connect(self.handle_stdout)
+            self.p.readyReadStandardError.connect(self.handle_stderr)
+            self.p.stateChanged.connect(self.handle_state)
+            self.p.finished.connect(self.process_finished)  # Clean up once complete.
+            self.p.start("python3", ['../socket_server.py'])
+            self.main_window.control_panel.raw_data_panel.btn.setText('Stop server')
+        else:
+            self.p.terminate()
+            self.main_window.control_panel.raw_data_panel.btn.setText('Start server')
+
+    def handle_stderr(self):
+        data = self.p.readAllStandardError()
+        stderr = bytes(data).decode("utf8")
+        self.message(stderr)
+        # Extract progress if it is in the data.
+        # progress = simple_percent_parser(stderr)
+        # if progress:
+        #     self.progress.setValue(progress)
+        # self.message(stderr)
+
+    def handle_stdout(self):
+        data = self.p.readAllStandardOutput()
+        stdout = bytes(data).decode("utf8")
+        self.message(stdout)
+
+    def handle_state(self, state):
+        states = {
+            QProcess.NotRunning: 'Not running',
+            QProcess.Starting: 'Starting',
+            QProcess.Running: 'Running',
+        }
+        state_name = states[state]
+        self.message(f"State changed: {state_name}")
+
+    def process_finished(self):
+        self.message("Process finished.")
+        self.main_window.control_panel.raw_data_panel.btn.setText('Start server')
+        self.p = None
+
+class AcquisitionWorker(QObject): 
+    #finished  = Signal()
+    data_ready = Signal()
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def run(self):
+        pass
---
+++ src/main_window.py
@@ -0,0 +1,124 @@
+#!/home/apehooman/.virtualenvs/env/bin/python
+from PySide6.QtWidgets import (
+    QMainWindow, 
+    QPushButton, 
+    QWidget, 
+    QHBoxLayout, 
+    QVBoxLayout, 
+    QGroupBox, 
+    QLabel, 
+    QFrame,
+    QPlainTextEdit)
+import numpy as np
+import pyqtgraph as pg
+
+
+class GraphScreen(pg.PlotWidget):
+    def __init__(self, controller, parent=None, background="w", plotItem=None, **kargs):
+        super().__init__(parent=parent, background=background, plotItem=plotItem, **kargs)
+
+        styles = {"color": "k", "font-size": "12px"}
+        self.controller = controller
+        self.setLabel("left", "Current" ,"mA", **styles)
+        self.setLabel("bottom", "Time" ,"s", **styles)
+        self.showGrid(x=True,y=True)
+        self.pen_ch1 = pg.mkPen(color="b", width=1)
+        # self.setXRange(0,1, padding=0.02)
+        self.setXRange(-10, 0)
+        # self.setYRange(0, 5, padding=0.02)
+        self.chunkSize = 1000
+        self.data = np.empty((self.chunkSize+1, 2))
+        self.ptr = 0
+        self.curves = []
+        self.startTime = pg.ptime.time()
+        self.maxChunks = 10
+    
+    def plot_ch(self, x, y, ch=1):
+        self.data_line_ch = self.plot(x, y, pen = self.pen_ch1)
+
+    def update_ch(self):
+        now = pg.ptime.time()
+        for c in self.curves:
+            c.setPos(-(now-self.startTime), 0)
+        i = self.ptr % self.chunkSize
+        if i == 0:
+            curve = self.plot(pen=self.pen_ch1)
+            self.curves.append(curve)
+            last = self.data[-1]
+            self.data = np.empty((self.chunkSize+1, 2))
+            self.data[0] = last
+            while len(self.curves) > self.maxChunks:
+                c = self.curves.pop(0)
+                self.removeItem(c)
+        else:
+            curve = self.curves[-1]
+        self.data[i+1,0] = now - self.startTime
+        self.data[i+1,1] = np.random.normal() # adds only one point but the ESP32 sends 30k at once 
+        curve.setData(x=self.data[:i+2, 0], y=self.data[:i+2, 1]) # plots all available
+        self.ptr += 1
+
+    def update(self):
+        self.update_ch()
+    
+    # def update_ch(self, x, y, ch = 1):
+    #     self.data_line_ch.setData(x, y)
+
+class StatsBox(QGroupBox):
+    def __init__(self) -> None: # may need change
+        super().__init__("Stats")
+        self.esp_addr_label  = QLabel("Mac")
+        
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(QLabel("Sensor Address:"))
+        layout.addWidget(self.esp_addr_label)
+
+class RawDataBox(QGroupBox):
+    def __init__(self, controller, parent=None) -> None:
+        super().__init__("Raw Data", parent=parent)
+        self.controller = controller
+
+        self.txt_box = QPlainTextEdit()
+        self.txt_box.setReadOnly(True) 
+        self.btn = QPushButton("Start Server")
+        self.btn.pressed.connect(self.controller.start_process)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(self.txt_box)
+        layout.addWidget(self.btn)
+        
+class ControlPanel(QFrame):
+    def __init__(self, controller, parent = None ):
+        super().__init__(parent=parent)
+        self.controller = controller
+        self.setFrameStyle(QFrame.StyledPanel)
+        
+        self.stats_panel = StatsBox()
+        self.raw_data_panel = RawDataBox(self.controller)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.stats_panel)
+        self.layout.addStretch()  
+        self.layout.addWidget(self.raw_data_panel)
+        ''' adds space between boxes'''
+        self.setMaximumWidth(350)
+        self.setLayout(self.layout)
+
+class MainWindow(QMainWindow):
+    def __init__(self, controller, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.controller = controller
+        self.setupUi()
+
+    def setupUi(self):
+        self.setWindowTitle("Current Sensor")
+
+        self.screen = GraphScreen(self.controller)
+        self.control_panel = ControlPanel(self.controller)
+        self.content_layout = QHBoxLayout()
+        self.content_layout.addWidget(self.screen)
+        self.content_layout.addWidget(self.control_panel)
+        # self.setMinimumSize(QSize())
+        self.setCentralWidget(QWidget())
+        self.centralWidget().setLayout(self.content_layout)
